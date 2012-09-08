cd9f1fb50a5a616a6c64a756d66a9aa36b398f34
Honza Javorek <jan.javorek@gmail.com>
2012-09-08 23:29:31 +0200

    console app entry point

--- setup.py
+++ setup.py
@@ -19,5 +19,10 @@ setup(
     license='ISC',
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
-    install_requires=requirements
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'zitkino = zitkino:main',
+        ],
+    }
 )
--- zitkino/__init__.py
+++ zitkino/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 
-import os
 from flask import Flask
 from flask_heroku import Heroku
 
@@ -15,7 +14,5 @@ def hello():
     return 'Hello!'
 
 
-if __name__ == '__main__':
-    # bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+def main():
+    pass
