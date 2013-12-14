da8e638beec62f97b4957617357752cfaebf55c0
Honza Javorek <jan.javorek@gmail.com>
2013-12-14 20:25:41 +0100

    Tuning of deployment process. MANIFEST.in was redundant since e630724a861c37d4a7b692d6aaea8c34dbddd4fe.

--- fabfile.py
+++ fabfile.py
@@ -71,7 +71,8 @@ def deploy():
         local('git checkout deploy')
 
         # build and commit static files
-        local('python manage.py assets -q --parse-templates build')
+        with shell_env(PYTHONDONTWRITEBYTECODE=1):
+            local('python manage.py assets -q --parse-templates build')
         local('git add --force ' + os.path.join(static_dir, 'css/packed.css'))
         local('git add --force ' + os.path.join(static_dir, 'js/packed.js'))
         local('git commit -m "Static files."')
